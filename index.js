27f25433e090fb3bd472083bea376e6b9151d285
marijedb <marijedb@live.nl>
2022-11-19 06:01:33 +0100

    updated render function

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
 const searchInput = document.getElementById('search-input')
 const btnSearch = document.getElementById('btn-search')
+const mainContent = document.getElementById('main')
 
-btnSearch.addEventListener("click", function(e) {
-    e.preventDefault()
+let imdbMovieList = []
+
+btnSearch.addEventListener("click", getMovieId)
+
+function getMovieId(event) {
+    event.preventDefault()
     fetch(`https://www.omdbapi.com/?apikey=72add8e3&s=${searchInput.value}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
-            fetch(`https://www.omdbapi.com/?apikey=72add8e3&i=${data.Search[0].imdbID}`)
-                .then(res => res.json())
-                .then(data => console.log(data))
+            if (!data.Search) {
+                console.log(data.Error)
+            } else {
+                for (movie of data.Search) {
+                    imdbMovieList.push(movie.imdbID)
+                }
+                console.log(imdbMovieList)
+                renderMovies()
+            }
         })
-})
+}
 
 
+function renderMovies() {
+    let movieHtml = ""
+    
+    imdbMovieList.forEach(function (id) {
+        fetch(`https://www.omdbapi.com/?apikey=72add8e3&i=${id}`)
+            .then(res => res.json())
+            .then(data => {
+                movieHtml += `<div>
+                            <h3>${data.Title}</h3>
+                            <p>${data.Runtime}</p>
+                        </div>
+                        `
+                // console.log("Title:", data.Title)
+                // console.log("IMDB Rating:", data.imdbRating)
+                // console.log("Runtime:", data.Runtime)
+                // console.log("Genre:", data.Genre)
+                // console.log("Plot:", data.Plot)
+                console.log(movieHtml)
+                mainContent.innerHTML = movieHtml
+            })
+            
+        })
+}
 
 // When search button is clicked:
 // - it will fetch the movie list from the API
 // - checks if something comes back. If not display on page
 // - if true get the 10 items and store their imdbID  in an array?
 // - fetch all imdb data and render them to the page. 
+
+// imdbID.Title
+// imdbID.imdbRating
+// imdbID.Runtime
+// imdbID.Genre
+// imdbID.Plot
+// imdbID.Poster
