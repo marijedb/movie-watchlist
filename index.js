57f64f18b935b94287c946c975a130b85c391d6a
marijedb <marijedb@live.nl>
2022-11-22 03:12:01 +0100

    added add to watchlist functionality

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const btnSearch = document.getElementById('btn-search')
 const mainContent = document.getElementById('main')
 
 let imdbMovieList = []
+let watchList = []
 
 btnSearch.addEventListener("click", getMovieId)
+
+document.addEventListener("click", function(event){
+    if(event.target.dataset.id){
+        addToWatchlist(event.target.dataset.id)
+    } else if(event.target.dataset.watchlist){
+        let test = JSON.parse(localStorage.getItem("movies"))
+        console.log(test)
+    }
+})
+
 
 function getMovieId(event) {
     event.preventDefault()
@@ -17,42 +28,71 @@ function getMovieId(event) {
                 for (movie of data.Search) {
                     imdbMovieList.push(movie.imdbID)
                 }
-                console.log(imdbMovieList)
                 renderMovies()
             }
         })
 }
 
+function addToWatchlist(movieID){
+    watchList.push(movieID)
+    localStorage.setItem("movies", JSON.stringify(watchList));
+    document.getElementById(`${movieID}`).innerHTML = `<p class="added-msg">Added to watchlist</p>`
+}
+
 
 function renderMovies() {
     let movieHtml = ""
-    
+    if(JSON.parse(localStorage.getItem("movies"))) {
+        watchList = JSON.parse(localStorage.getItem("movies"))
+    }
     imdbMovieList.forEach(function (id) {
         fetch(`https://www.omdbapi.com/?apikey=72add8e3&i=${id}`)
             .then(res => res.json())
             .then(data => {
-                movieHtml += `
-                    <div class="search-result-container">
-                        <img class="movie-poster" src="${data.Poster}">
-                        <div class="movie-info">
-                            <div class="movie-header">
-                                <h3 class="movie-title">${data.Title}</h3>
-                                <p><img src="./images/star.png"> ${data.imdbRating}</p>
-                            </div>
-                            <div class="movie-sub-info">
-                                <p>${data.Genre}</p>
-                                <p class="runtime-info">${data.Runtime}</p>
-                                <div class="add-watchlist">
-                                    <img class="icon" src="./images/plus.png">
-                                    <p>Watchlist</p>
+                if(!watchList.includes(id)){
+                    movieHtml += `
+                        <div class="search-result-container">
+                            <img class="movie-poster" src="${data.Poster}">
+                            <div class="movie-info">
+                                <div class="movie-header">
+                                    <h3 class="movie-title">${data.Title}</h3>
+                                    <p><img src="./images/star.png"> ${data.imdbRating}</p>
                                 </div>
+                                <div class="movie-sub-info">
+                                    <p>${data.Genre}</p>
+                                    <p class="runtime-info">${data.Runtime}</p>
+                                    <div id="${id}" class="add-watchlist">
+                                        <img class="icon" src="./images/plus.png" data-id="${id}">
+                                        <p data-id="${id}">Watchlist</p>
+                                    </div>
+                                </div>
+                                <p class="plot">${data.Plot}</p>
                             </div>
-                            <p class="plot">${data.Plot}</p>
                         </div>
-                    </div>
-                    <hr class="divider">
-                    `
-                console.log(movieHtml)
+                        <hr class="divider">
+                        `
+                } else {
+                    movieHtml += `
+                        <div class="search-result-container">
+                            <img class="movie-poster" src="${data.Poster}">
+                            <div class="movie-info">
+                                <div class="movie-header">
+                                    <h3 class="movie-title">${data.Title}</h3>
+                                    <p><img src="./images/star.png"> ${data.imdbRating}</p>
+                                </div>
+                                <div class="movie-sub-info">
+                                    <p>${data.Genre}</p>
+                                    <p class="runtime-info">${data.Runtime}</p>
+                                    <div id="${id}" class="add-watchlist">
+                                        <p class="added-msg">Added to watchlist</p>
+                                    </div>
+                                </div>
+                                <p class="plot">${data.Plot}</p>
+                            </div>
+                        </div>
+                        <hr class="divider">
+                        `
+                }
                 mainContent.innerHTML = movieHtml
             })
             
---
+++ watchlist.js
@@ -0,0 +1,41 @@
+const searchInput = document.getElementById('search-input')
+const btnSearch = document.getElementById('btn-search')
+const mainContent = document.getElementById('main')
+let watchList = JSON.parse(localStorage.getItem("movies"))
+
+function renderWatchList(){
+    let watchlistHtml = ``
+
+    watchList.forEach(function(movieId){
+        fetch(`https://www.omdbapi.com/?apikey=72add8e3&i=${movieId}`)
+            .then(response => response.json())
+            .then(data => {
+                watchlistHtml += `
+                    <div class="search-result-container">
+                        <img class="movie-poster" src="${data.Poster}">
+                        <div class="movie-info">
+                            <div class="movie-header">
+                                <h3 class="movie-title">${data.Title}</h3>
+                                <p><img src="./images/star.png"> ${data.imdbRating}</p>
+                            </div>
+                            <div class="movie-sub-info">
+                                <p>${data.Genre}</p>
+                                <p class="runtime-info">${data.Runtime}</p>
+                                <div id="${movieId}" class="add-watchlist">
+                                    <img class="icon" src="./images/minus.png" data-id="${movieId}">
+                                    <p data-id="${movieId}">Delete</p>
+                                </div>
+                            </div>
+                            <p class="plot">${data.Plot}</p>
+                        </div>
+                    </div>
+                    <hr class="divider">`
+
+                mainContent.innerHTML = watchlistHtml
+            })
+    })
+}
+
+if(watchList){
+    renderWatchList()
+}
